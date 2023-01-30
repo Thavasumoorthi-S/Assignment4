74361545bedd697aa06b9aefca48ca1191947f90
Thavasumoorthi-S <thavasumoorthi.s@tringapps.com>
2023-01-30 11:39:47 +0530

    Fouth c ommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,55 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+class Singleton
+{
+    private static final Logger LOGGER=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private static Singleton singleton=null;
+    private Singleton()
+    {
+
+    }
+    public static Singleton getinstance()
+    {
+        if(singleton==null)
+        {
+            singleton=new Singleton();
+        }
+        LOGGER.log(Level.INFO,Singleton.Connection());
+        return singleton;
+    }
+    public static String Connection()
+    {
+        return "Connected";
+    }
+    public static String Close()
+    {
+        Singleton singleton=null;
+        return "connection closed";
+    }
+    public static void main(String[] args)
+    {
+        int choice;
+        Scanner sc=new Scanner(System.in);
+        while(true)
+        {
+            LOGGER.log(Level.INFO,"\n1)Connection make \n2)connection close \n3)exit");
+            LOGGER.log(Level.INFO,"Choose any one of the option in the above: ");
+            choice=sc.nextInt();
+            if(choice==1)
+            {
+                Singleton singleton=Singleton.getinstance();
+                LOGGER.log(Level.INFO,"",singleton.hashCode());
+            }
+            else if(choice==2)
+            {
+                LOGGER.log(Level.INFO,Singleton.Close());
+            }
+            else if(choice==3)
+            {
+                break;
+            }
+        }
     }
 }
