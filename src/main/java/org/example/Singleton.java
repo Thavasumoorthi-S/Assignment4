282e23ba3ab5a0e4307d74f81a43445b84b66742
Thavasumoorthi-S <thavasumoorthi.s@tringapps.com>
2023-01-31 11:09:07 +0530

    Fouth c ommit

--- src/main/java/org/example/Singleton.java
+++ src/main/java/org/example/Singleton.java
@@ -2,21 +2,21 @@ package org.example;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-class Singleton
+class SingletonException
 {
     private static final Logger LOGGER=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    private static Singleton singleton=null;
+    private static SingletonException singleton=null;
     String a;
     private static int flag=0;
-    private Singleton()
+    private SingletonException()
     {
 
     }
-    public static Singleton getinstance()
+    public static SingletonException getinstance()
     {
         if(singleton==null)
         {
-            singleton=new Singleton();
+            singleton=new SingletonException();
             flag=1;
         }
         return singleton;
@@ -40,25 +40,20 @@ class Singleton
         Scanner sc=new Scanner(System.in);
         while(true)
         {
-            try {
                 LOGGER.log(Level.INFO, "\n1)Connection make \n2)connection close \n3)exit");
                 LOGGER.log(Level.INFO, "Choose any one of the option in the above: ");
                 choice = sc.nextInt();
                 if (choice == 1) {
-                    Singleton singleton = Singleton.getinstance();
+                    SingletonException singleton = SingletonException.getinstance();
                     int address = singleton.hashCode();
                     LOGGER.log(Level.INFO, Integer.toString(address), address);
                     connresult = singleton.connection();
                     LOGGER.info(connresult);
                 } else if (choice == 2) {
-                    result = Singleton.close();
+                    result = SingletonException.close();
                     LOGGER.info(result);
                 } else if (choice == 3) {
                     break;
-                }
-            }
-            catch(InputMismatchException e){
-                throw new InputMismatchException("Please Enter the valid inpuit ");
             }
         }
     }
